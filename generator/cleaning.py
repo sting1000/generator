9f9e79d9bf7452ca9891b4eea68550726478ece4
Su <zhechensu@gmail.com>
2020-10-31 21:35:55 +0100

    modify generator to speed up, add prepare.py and config

---
+++ generator/cleaning.py
@@ -0,0 +1,27 @@
+import unicodedata
+import re
+
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([a-zA-Z]+)[\:\-\'\.]", r"\1 ", s)  # colun: dsfa -> colun dsfa
+    s = re.sub(r"[()#\-]", r" ", s)  # colun-dsfa -> colun dsfa
+    s = re.sub(r"([\+])", r" \1 ", s)
+    s = re.sub(r'\s+', ' ', s)
+    s = s.strip()
+    return s
+
+
+def remove_noisy_tags(text: str) -> str:
+    """
+    Removes the CH, D, F, I, HD tags from the string.
+    """
+    text = re.sub(r'\b(?:)(CH|DE|FR|HD|UHD)\b', '', text, flags=re.IGNORECASE)
+    return text
--- generator/module.py
+++ generator/module.py
@@ -1,6 +1,6 @@
 from generator.tools import filter_aliases, remove_sharp_sign, assign_tag_to_words, make_bio_tag
 from generator.normalizer import Normalizer
-from tqdm import tqdm
+from generator.cleaning import normalizeString
 import random
 import re
 
@@ -18,7 +18,9 @@ class Generator:
         self.entities = entities
         self.normalizer = Normalizer().normalize_text
         self.method = method
-        self.command_pool = []
+        self.command_pool = {}
+        self.tag_dic = {}
+        self.template_dic = {}
 
     def get_values_from_tag(self, tag: str, target_lang: str) -> list:
         """
@@ -31,26 +33,19 @@ class Generator:
         Returns: list of possible value(s)
 
         """
-        # get entity
-        if tag not in self.entities['type'].values:
-            print('ERROR: Tag {} does not exist, try anther one.'.format(tag))
-            return
-        if target_lang not in self.entities['language'].values:
-            print('ERROR: Language {} does not exist, try anther one.'.format(target_lang))
-            return
+        if tag not in self.tag_dic:
+            # TODO: comment language selection to have more permutation
+            selected_entities = self.entities[self.entities['type'] == tag][self.entities['language'] == target_lang]
 
-        # TODO: comment language selection to have more permutation
-        selected_entities = self.entities[self.entities['type'] == tag][self.entities['language'] == target_lang]
-
-        filtered_values = []
-        # find all values in the entity
-        for _, selected_entity in selected_entities.iterrows():
-            aliases = selected_entity['aliases']
-            value = [str(selected_entity['value'])]
-            normalized_value = [selected_entity['normalizedValue']]
-            filtered_values += filter_aliases(aliases + value + normalized_value, target_lang)
-
-        selected_values = self.apply_method(filtered_values)
+            filtered_values = []
+            # find all values in the entity
+            for _, selected_entity in selected_entities.iterrows():
+                aliases = selected_entity['aliases']
+                value = [str(selected_entity['value'])]
+                normalized_value = [selected_entity['normalizedValue']]
+                filtered_values += filter_aliases(aliases + value + normalized_value, target_lang)
+            self.tag_dic[tag] = filtered_values
+        selected_values = self.apply_method(self.tag_dic[tag])
         return selected_values
 
     def get_templates(self, target_id: str, target_lang: str) -> list:
@@ -63,8 +58,11 @@ class Generator:
         Returns: list of template(s)
 
         """
-        pattern_list = self.templates[self.templates['id'] == target_id][target_lang].values[0]['texts']
-        return self.apply_method([p['ttsText'] for p in pattern_list])
+        if target_id not in self.template_dic:
+            pattern_list = self.templates[self.templates['id'] == target_id][target_lang].values[0]['texts']
+            pattern_list = [p['ttsText'] for p in pattern_list]
+            self.template_dic['target_id'] = pattern_list
+        return self.apply_method(self.template_dic['target_id'])
 
     def get_command(self, target_id: str, target_lang: str, verbose=False) -> list:
         """
@@ -83,7 +81,7 @@ class Generator:
             print("Choose template: \n\t{}".format(templates))
 
         for template in templates:
-            template = [token.strip('#') for token in template.split()]
+            template = template.split()  # [token.strip('#') for token in template.split()]
             template_command_pool = []
 
             def replace_pos(pos: int, curr_temp: list, curr_label: list):
@@ -94,23 +92,24 @@ class Generator:
                 if re.findall(r'{\S+}', template[pos]):
                     tag = template[pos][1: -1]
                     for selected_value in self.get_values_from_tag(tag, target_lang):
-                        selected_value = [token.strip('#') for token in selected_value.split()]
+                        selected_value = selected_value.split()  # [token.strip('#') for token in selected_value.split()]
                         for token in selected_value:
                             curr_label.extend(self.normalizer(token, target_lang).split())
                         curr_temp.extend(selected_value)
                         replace_pos(pos + 1, curr_temp, curr_label)
                 else:
-                    # curr_temp.append(template[pos])
-                    # curr_label.append('O')
                     curr_temp.append(template[pos])
                     curr_label.append(template[pos])
                     replace_pos(pos + 1, curr_temp, curr_label)
 
             replace_pos(0, [], [])
-            self.command_pool += template_command_pool
+            # self.command_pool.update(template_command_pool)
         if verbose:
             print("After tag removal: \n\t{}".format(self.command_pool))
-        return template_command_pool #self.command_pool
+
+        template_command_pool = [(normalizeString(' '.join(pair[0])), normalizeString(' '.join(pair[1]))) for pair in
+                                 template_command_pool]
+        return template_command_pool  # self.command_pool
 
     def apply_method(self, li: list) -> list:
         """
