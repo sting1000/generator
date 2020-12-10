f364a3c5511630dadc71fa67619e9d84339be049
Su <zhechensu@gmail.com>
2020-12-10 15:55:33 +0100

    add exp folder, delete ipynb

--- predict.py
+++ predict.py
@@ -1,14 +1,15 @@
 import os
 
+# set path
 project_path = '/mnt/workspace/project'
 onmt_path = '/mnt/workspace/OpenNMT-py'
 exp_path = project_path + '/exp'
 
+# set model parameters
 chosen_lan = 'it'
 model_name = 'BiLSTM_char_LSTM_char_it'
 steps = 100000
 enc_level = 'char'
-
 output_file = 'pred_{model_name}_{steps}_steps.txt'.format(model_name=model_name, steps=steps)
 src_file = '{exp_path}/data_{lan}/src_test_{enc_level}.txt'.format(lan=chosen_lan, exp_path=exp_path, enc_level=enc_level)
 
--- train.py
+++ train.py
@@ -1,15 +1,7 @@
 import os
-
 from helper import *
 from tqdm import tqdm
 from pathlib import Path
-
-
-
-# augment training
-# from imblearn.over_sampling import RandomOverSampler
-# ros = RandomOverSampler(random_state=0)
-# train, _= ros.fit_resample(train, train['language'])
 
 
 # take specific lan
@@ -17,25 +9,22 @@ chosen_lan = 'it'
 project_path = '/mnt/workspace/project'
 onmt_path = '/mnt/workspace/OpenNMT-py'
 encoder_level = 'char'
-decoder_level = 'char'
-rnn = 'lstm' #'transformer'
+model_type = 'BiLSTM_char_LSTM_char'
 
 train = read_data_json("data/nmt_data_json/train_train.json")
 test = read_data_json("data/nmt_data_json/test_test.json")
 valid = read_data_json("data/nmt_data_json/valid_valid.json")
 exp_path = project_path + '/exp'
 data_output_dir = exp_path + 'data/'
-########
+yaml_path = '{exp_path}/yaml/{model_type}.yaml'.format(exp_path=exp_path, model_type=model_type)
 
-
+# read dataset
 train = train[train['language'] == chosen_lan]
 test = test[test['language'] == chosen_lan]
 valid = valid[valid['language'] == chosen_lan]
 
-
-Path(data_output_dir).mkdir(parents=True, exist_ok=True)
-
 # generate pairs for training
+Path(data_output_dir).mkdir(parents=True, exist_ok=True)
 for appendix in ['_char', '_token']:
     f_src_test = open(data_output_dir + 'src_test' + appendix + '.txt', "w")
     f_tgt_test = open(data_output_dir + 'tgt_test' + appendix + '.txt', "w")
@@ -44,7 +33,7 @@ for appendix in ['_char', '_token']:
     f_src_train = open(data_output_dir + 'src_train' + appendix + '.txt', "w")
     f_tgt_train = open(data_output_dir + 'tgt_train' + appendix + '.txt', "w")
 
-    # generate
+    # write to files
     for name, df, f_src, f_tgt in [('train', train, f_src_train, f_tgt_train),
                      ('test', test, f_src_test, f_tgt_test),
                      ('valid', valid, f_src_val, f_tgt_val)]:
@@ -59,16 +48,8 @@ for appendix in ['_char', '_token']:
     f_src_train.close()
     f_tgt_train.close()
 
-# get model name
-if rnn == 'lstm':
-    model_name = "BiLSTM_{encoder_level}_LSTM_{decoder_level}".format(encoder_level=encoder_level, decoder_level=decoder_level)
-elif rnn == 'transformer':
-    model_name = 'transformer_{encoder_level}'.format(encoder_level=encoder_level)
-yaml_path = '{exp_path}/yaml/{model_name}_train.yaml'.format(exp_path=exp_path, model_name=model_name)
-
+# train
 command_build_vocab = "python {onmt_path}/build_vocab.py -config  {yaml_path} -n_sample -1".format(onmt_path=onmt_path, yaml_path=yaml_path)
 command_train = "python {onmt_path}/train.py -config {yaml_path}".format(onmt_path=onmt_path, yaml_path=yaml_path)
-
-# train
 os.system(command_build_vocab)
 os.system(command_train)
